20959cfc8b95d0c7b5e72ceebfd26ef21e8e5222
Pavlin Nedelchev <minimaanima@gmail.com>
2018-08-20 20:22:38 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'workshop';
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAfSsum37ZLs95LTdEc57nOA5JHCFFvS4E",
+      authDomain: "ng-recipes-32b7d.firebaseapp.com",
+  }
+  
+}
