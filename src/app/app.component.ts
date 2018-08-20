496e8123a95f905e73dab8367e4f43f30a39ac16
Pavlin Nedelchev <minimaanima@gmail.com>
2018-08-20 20:37:31 +0300

    fixed br

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent implements OnInit {
     firebase.initializeApp({
       apiKey: "AIzaSyAfSsum37ZLs95LTdEc57nOA5JHCFFvS4E",
       authDomain: "ng-recipes-32b7d.firebaseapp.com",
+    })
   }
-  
 }
