723e03df46d3bf4bacb73791679a81852b1531c5
Pavlin Nedelchev <minimaanima@gmail.com>
2018-08-20 20:34:31 +0300

    Added authService
    sign in and sign up

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    signUp(email: string, password: string) {
+        firebase.auth()
+        .createUserWithEmailAndPassword(email,password)
+        .then((data)=> console.log(data))
+        .catch( (err) => console.log(err))
+    };
+
+    signIn(email: string, password: string) {
+        firebase.auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((data)=> console.log(data))
+        .catch( (err) => console.log(err))
+    }
+}
